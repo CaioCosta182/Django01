08dbc3ebb669348d99d74dd1eaf4b4e0085867dd
CaioCosta182 <107070158+CaioCosta182@users.noreply.github.com>
2023-02-10 08:41:40 -0300

    Update settings.py
    Sec update

--- udemydrf/settings.py
+++ udemydrf/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vcnuh+x!7q9a0*phj@3nqdcn5@u%rr8yqxxg5)-b$3)_a5o13u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
